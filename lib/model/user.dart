1391702f905b044830a24718ab8918ec5e844f41
Sakyo24 <sakyo.work24@gmail.com>
2023-10-16 23:25:10 +0900

    グループ詳細の作成

---
+++ lib/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+  int id;
+  String name;
+  String email;
+  String created_at;
+  String updated_at;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.created_at,
+    required this.updated_at,
+  });
+}
